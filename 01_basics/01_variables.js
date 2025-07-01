b9e3a52fe0fda5776f3521ff5ef1ac370216db2e
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-07-01 05:00:45 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,7 +2,11 @@ const accountId = 144322
 let accountEmail ="rabbi@gmail.com"
 var accountPassword ="134256"
 accountCity="jaipur"
+accountState ;
 
 console.log(accountEmail);
-console.table([accountId,accountPassword,accountCity,accountEmail]);
+console.table([accountId,accountPassword,accountCity,accountEmail,accountState]);
 
+/*
+prefer not to use var 
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3)// we are using node, not browser
+
+console.log(3+3);
+console.log("Hey there, it's fun using JS....");
+
+var name="Rabbi"
+var age=19
+var isLoggedin = false
+
+//datatypes:
+//number=> range 2 to the power 53
+//bigint=> used for big numbers like trading... 
+// string ""
+// boolean=> true/false
+// null=> a stand alone value
+// undefined=> value not assigned
+// symbol=> uniqueness
+
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
