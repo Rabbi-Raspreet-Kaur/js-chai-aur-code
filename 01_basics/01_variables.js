cf87a6eed73248e650bdb9b8dc94d39fb9a49bb6
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-07-01 04:28:00 +0000

    learning basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144322
+let accountEmail ="rabbi@gmail.com"
+var accountPassword ="134256"
+accountCity="jaipur"
+
+console.log(accountEmail);
+console.table([accountId,accountPassword,accountCity,accountEmail]);
+
