c5930e15dfde6e4c8df877502aa93ace6f40188a
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-07-01 06:14:31 +0000

    operations....

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = undefined
+
+console.log(typeof score);
+console.log(typeof(score)); //used as a method
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+let isLoggedIn = ""  //input value
+let booleanIsLoggedIn = Boolean(isLoggedIn)  //converted value into boolean
+console.log(booleanIsLoggedIn);  //print to check output // false
+
+
+let isLoggedIn2 = "rabbi"  //input value
+let booleanIsLoggedIn2 = Boolean(isLoggedIn2)  //converted value into boolean
+console.log(booleanIsLoggedIn2); //true value
+
+
+//**************  operations ****************
+
+
+console.log( ( 3 + 4 ) * 5 % 3);
+
+let gamecounter=100
+//gamecounter++;
+//console.log(gamecounter);// gives 101
+
+
+++gamecounter;
+console.log(gamecounter);
+
+
