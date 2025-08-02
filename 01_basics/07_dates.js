f49d7e88fd621368047707a9b1ca0f4afb95ead0
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-08-02 07:36:59 +0000

    dates update

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -1,5 +1,12 @@
 //  dates
 
 let myDate = new Date()
-console.log(myDate.toString());
+//console.log(myDate.toString());     //  Sat Aug 02 2025 06:46:48 GMT+0000 (Coordinated Universal Time)
+//console.log(myDate.toDateString());   //   Sat Aug 02 2025
+//console.log(myDate.toLocaleString());   //   8/2/2025, 6:46:48 AM
+
+//console.log(typeof myDate);      //  object
+
+let myCreatedDate = new Date(2023, 0, 23)
+console.log(myCreatedDate);
 
