b5a95ab5d2a8342657915d119cc58b78fa43a9b3
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-09-02 09:12:26 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,16 @@
+//singleton
+
+//object literals
+
+const JsUser1 ={}
+const JsUser ={
+    name: "rabbi",
+    age: 18,
+    location: "Jaipur",
+    email: "abc@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(JsUser.email);
+console.log(JsUser["email"]);
+
