60db82d69adab20e3460905761802aeee19eb6fa
Rabbi-Raspreet-Kaur <kaurrabbiraspreet@gmail.com>
2025-09-02 10:03:15 +0000

    obj_part1

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,16 +1,27 @@
 //singleton
+//object.create
 
 //object literals
 
-const JsUser1 ={}
+const mySym = Symbol("key1")
+
+
 const JsUser ={
     name: "rabbi",
+    "full name": "rabbi raspreet",
+    [mySym]: "mykey1",            //  referring as symbol so written in square brackets
     age: 18,
     location: "Jaipur",
     email: "abc@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
 console.log(JsUser.email);
 console.log(JsUser["email"]);
+console.log(typeof JsUser[mySym]);          // shows string
+
+JsUser.email = "abc@chatgpt.com"
+Object.freeze(JsUser)                    //changes above key value i.e. email changes
+console.log(JsUser);
 
